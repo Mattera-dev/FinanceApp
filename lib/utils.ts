7010d3b92d6495c42b45a5be3ca3b974c5b76b8c
mattera-dev <vinimatteraz@gmail.com>
2025-08-29 17:25:39 -0300

    feat: add functions to get token from request

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,11 +1,31 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 import jwt from "jsonwebtoken"
+import { NextRequest } from "next/server"
+import { IUserId } from "@/types/user"
+
+const JWT_KEY = process.env.JWT_KEY as string
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
 export function createJWT(obj: Object) {
-  return jwt.sign(obj, process.env.JWT_KEY as string)
+  return jwt.sign(obj, JWT_KEY)
+}
+
+export function getDataFromToken(req: NextRequest): null | IUserId {
+  const token = req.cookies.get("auth_token")?.value
+  if (!token) {
+    return null
+  }
+
+  try {
+    const userData = jwt.verify(token, JWT_KEY) as IUserId;
+    return userData
+  } catch (error) {
+    return null
+
+  }
+
 }
